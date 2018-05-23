c8c5621b5cc7fe2122450755330a061277de2131
Kangyun Wang <wky@Kangyuns-MacBook-Pro.local>
2018-05-23 15:25:59 +0200

    Tried a flowtype out

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,16 @@
+// @flow
+
 import React from 'react';
 
 import Link from 'gatsby-link';
 
 import './index.css';
 
-function Header({ siteTitle }) {
+type Props = {
+	siteTitle: string,
+ };
+
+function Header({ siteTitle }: Props) {
 	return (
 		<header>
 			<div>
