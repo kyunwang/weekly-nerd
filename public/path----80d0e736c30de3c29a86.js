f63252a84c1face83d5ee489c9413366372d58ba
Kangyun Wang <kevinwang8011@hotmail.com>
2018-06-17 14:55:20 +0200

    Remove public folder

--- public/path----80d0e736c30de3c29a86.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{107:function(t,e){t.exports={data:{site:{siteMetadata:{title:"The Little Tech Blog"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----80d0e736c30de3c29a86.js.map
